52e8e58d0bc241bc0fc73bca482f319df1d62ff1
Tony Mroczkowski <tony.mroczkowski@gmail.com>
2015-08-23 22:35:56 -0400

    some notebook updates

--- code/sensitivity.py
+++ code/sensitivity.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-import numpy as n
+import numpy as np
 
 k_b = 1.380658e-16
 
@@ -7,5 +7,5 @@ def Tsky(nu):
     return 237 * (nu/.150)**-2.5
 
 def sense(d, N, nu, B=100e6, t=3600):
-    return 2 * k_b * Tsky(nu) / (N * d**2 * n.sqrt(B*t) * 1e-23)
-
+    return 2 * 1.22**2 * np.pi / (4 *np.log(2)) *  k_b * Tsky(nu) / (N * d**2 * np.sqrt(B*t) * 1e-23)
+#    return 2 * k_b * Tsky(nu) / (N * d**2 * np.sqrt(B*t) * 1e-23)
